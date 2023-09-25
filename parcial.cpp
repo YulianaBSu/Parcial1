baddaac49e09e0fa9a7fd973084378f65750879f
YulianaBSu <yuliana.betancurs@udea.edu.co>
2023-09-24 23:56:03 -0500

    "Cambio de tipo de variable int por byte para ahorro de memoria, ingreso de patron 4 en funcion patrones y correccion de posiciones para funcion imagen"

--- parcial.cpp
+++ parcial.cpp
@@ -1,10 +1,10 @@
 // Definicion de pines para un Arduino Uno
-int pinData = 11;   // Pin de datos
-int pinLatch = 8;  // Pin de almacenamiento 
-int pinClock = 12;  // Pin de reloj
-int opc = 0;
-const int filas = 8;
-const int columnas = 8;
+byte pinData = 11;   // Pin de datos
+byte pinLatch = 8;  // Pin de almacenamiento 
+byte pinClock = 12;  // Pin de reloj
+byte opc = 0;
+const byte filas = 8;
+const byte columnas = 8;
 
 void menu();
 void verificacion();
@@ -26,13 +26,13 @@ void loop(){
   opc = Serial.parseInt();
   
   if(opc==1){
-	for (int n=0; n<8; n++){
+	for (byte n=0; n<8; n++){
     verificacion();
 	}
   }
   
   else if(opc==2){
-    int fila, columna, rep, repi=0;
+    byte fila, columna, rep, repi=0;
     Serial.println("Ingrese cantidad de posiciones que desea indicar: ");
     while (Serial.available() == 0);
     rep = Serial.parseInt();
@@ -96,11 +96,11 @@ void verificacion(){ //Funcion para encender todos los LEDs
 
 
 
-void imagen(int fila, int columna){
+void imagen(byte fila, byte columna){
   Serial.println("Patron ingresado");
   digitalWrite(pinLatch, LOW);
-  shiftOut(pinData, pinClock, MSBFIRST, ~(B00000001 << fila-1)); 
-  shiftOut(pinData, pinClock, MSBFIRST, B10000000 >> columna-1); 
+  shiftOut(pinData, pinClock, MSBFIRST, ~(B00000001 << columna-1)); 
+  shiftOut(pinData, pinClock, MSBFIRST, B10000000 >> fila-1); 
   digitalWrite(pinLatch, HIGH);
   delay(2000);
   digitalWrite(pinLatch, LOW);
@@ -110,13 +110,13 @@ void imagen(int fila, int columna){
 
 
 void patrones() {
-  int** leds = new int*[filas]; 
-  for (int i = 0; i < filas; i++) {
-    leds[i] = new int[columnas]();
+  byte** leds = new byte*[filas]; 
+  for (byte i = 0; i < filas; i++) {
+    leds[i] = new byte[columnas]();
   }
 //Patron1
-  for (int i = 0; i < filas; i++) {
-    for (int j = i; j < columnas - i; j++) {
+  for (byte i = 0; i < filas; i++) {
+    for (byte j = i; j < columnas - i; j++) {
       encenderLED(i + 4, j); 
       encenderLED(filas - i - 5, j); 
     }
@@ -124,15 +124,15 @@ void patrones() {
   delay(1000);
   
 //Patron2
-  for(int i = 0; i < filas; i++) {
+  for(byte i = 0; i < filas; i++) {
     encenderLED(i , i); 
     encenderLED(i, filas - i - 1); 
     }
   delay(1000);  
   
 //Patron3
-  for (int i = 0; i < filas; i++) {
-        for (int j = 0; j < columnas; j++) {
+  for (byte i = 0; i < filas; i++) {
+        for (byte j = 0; j < columnas; j++) {
             // Patrón de marcado
             if (
                 (i==0 || i==1 || i==4 || i==5) &&
@@ -150,10 +150,24 @@ void patrones() {
         }
     }
   delay(1000);
+
+//Patron4
+  for (byte i = 0; i < 4; i++) {
+    for (byte j = 0; j < 4; j++) {
+      encenderLED(i, i + j);
+    }
+  }
+
+  for (byte i = 4; i < filas; i++) {
+    for (byte j = 0; j < 4; j++) {
+      encenderLED(i, (j - i + columnas - 1));
+    }
+  }
+  
   
   digitalWrite(pinLatch, LOW);
-  shiftOut(pinData, pinClock, MSBFIRST, B11111111); // Apaga todas las columnas
-  shiftOut(pinData, pinClock, MSBFIRST, B00000000); // Apaga todas las filas
+  shiftOut(pinData, pinClock, MSBFIRST, B11111111); 
+  shiftOut(pinData, pinClock, MSBFIRST, B00000000); 
   digitalWrite(pinLatch, HIGH);
 
   // Liberar memoria
@@ -167,15 +181,15 @@ void publik(){
   Serial.println("c.Secuencia de patrones");
   
 }
-void encenderLED(int fila, int columna) {
+void encenderLED(byte fila, byte columna) {
   digitalWrite(pinLatch, LOW);
   shiftOut(pinData, pinClock, MSBFIRST, ~(1 << (7 - fila))); 
   shiftOut(pinData, pinClock, MSBFIRST, B10000000 >> columna);   
   digitalWrite(pinLatch, HIGH);
 }
 
-void liberarMemoria(int** leds) {
-  for (int i = 0; i < filas; i++) {
+void liberarMemoria(byte** leds) {
+  for (byte i = 0; i < filas; i++) {
     delete[] leds[i];
   }
   delete[] leds;
